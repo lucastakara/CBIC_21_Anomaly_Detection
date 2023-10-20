540f4c5ffa2983da26632368d1da096fbbb2f171
lucastakara <lucastakara2@gmail.com>
2023-10-20 18:00:04 -0300

    Created unitary tests for main.py

--- models_architecture/BILSTM_Autoencoder.py
+++ models_architecture/BILSTM_Autoencoder.py
@@ -1,35 +1,70 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, RepeatVector, TimeDistributed, Bidirectional
-from tensorflow import keras
-from tensorflow.keras.utils import plot_model
 
 
-class BiLSTM_Autoencoder:
-    def __init__(self, X_train, y_train, X_test, y_test):
-        self.model = self.create_model()
+class BiLSTMAutoencoder:
+    def __init__(self, input_shape):
+        """
+        Initializes the BiLSTM Autoencoder model.
 
-    @staticmethod
-    def create_model(time_window_size=30, n_features=1):
-        model_BiLSTM = Sequential()
-        model_BiLSTM.add(Bidirectional(LSTM(units=128, return_sequences=False),
-                                       input_shape=(time_window_size, n_features)))
-        model_BiLSTM.add(RepeatVector(time_window_size))
-        model_BiLSTM.add(TimeDistributed(Dense(n_features, activation="linear")))
-        plot_model(model_BiLSTM, to_file="/home/takara/Desktop/Masters/BR_Conference/images/model_BILSTM.png", show_shapes=True, show_layer_names=True)
-        model_BiLSTM.compile(optimizer='adam', loss='MAPE')
-        model_BiLSTM.summary()
+        :param input_shape: tuple, shape (time_window_size, n_features) for LSTM input.
+        """
+        self.model = self._create_model(input_shape)
 
-        return model_BiLSTM
+    def _create_model(self, input_shape):
+        """
+        Creates a BiLSTM Autoencoder model.
+
+        :param input_shape: tuple, shape (time_window_size, n_features) for BiLSTM input.
+        :return: Compiled BiLSTM Autoencoder model.
+        """
+        model = Sequential()
+        model.add(Bidirectional(LSTM(128, return_sequences=False), input_shape=input_shape))
+        model.add(RepeatVector(input_shape[0]))  # The 'time_window_size' dimension is the length of sequences.
+        model.add(Bidirectional(LSTM(128, return_sequences=True)))  # Adding a BiLSTM layer for decoding.
+        model.add(TimeDistributed(Dense(input_shape[1], activation="linear")))  # Assuming linear activation for reconstruction.
+
+        model.compile(optimizer='adam', loss='mae')  # Assuming Mean Absolute Error (mae) is appropriate.
+        return model
 
     def train(self, X_train, y_train, epochs=60, batch_size=32, validation_split=0.1, shuffle=False):
-        history = self.model.fit(x=X_train, y=y_train, epochs=epochs, batch_size=batch_size,
-                                  validation_split=validation_split, shuffle=shuffle)
+        """
+        Trains the model on the given dataset.
+
+        :param X_train: ndarray, training data.
+        :param y_train: ndarray, target values for training data.
+        :param epochs: int, number of epochs to train the model.
+        :param batch_size: int, number of samples per gradient update.
+        :param validation_split: float, fraction of the training data to be used as validation data.
+        :param shuffle: boolean, whether to shuffle the training data before each epoch.
+        :return: History object, details about the training history at each epoch.
+        """
+        history = self.model.fit(
+            x=X_train, y=y_train,
+            epochs=epochs,
+            batch_size=batch_size,
+            validation_split=validation_split,
+            shuffle=shuffle
+        )
         return history
 
-    def predict(self, X_train):
-        X_train_pred = self.model.predict(X_train)
-        return X_train_pred
+    def predict(self, data):
+        """
+        Generates output predictions for the input samples.
+
+        :param data: ndarray, input data.
+        :return: ndarray, predictions.
+        """
+        predictions = self.model.predict(data)
+        return predictions
 
     def evaluate(self, X_test, y_test):
-        mae = self.model.evaluate(X_test, y_test)
-        return mae
+        """
+        Evaluates the model performance on the test dataset.
+
+        :param X_test: ndarray, test data.
+        :param y_test: ndarray, target values for test data.
+        :return: Loss value on the test data.
+        """
+        loss = self.model.evaluate(X_test, y_test)
+        return loss
---
+++ tests/test_main.py
@@ -0,0 +1,81 @@
+import unittest
+from unittest.mock import Mock, patch
+import pandas as pd
+import numpy as np
+from main import plot_anomaly_points, plot_train_val_loss, \
+    determine_anomalies  # Adjust the import statement according to your file structure and actual file names.
+
+
+class TestPlotFunctions(unittest.TestCase):
+
+    @patch('main.plt.show')  # Mock 'plt.show' to avoid opening a window during testing
+    def test_plot_anomaly_points(self, mock_plt):
+        # Mock data - adjust as necessary to match what your function expects and uses
+        anomalies = pd.DataFrame({
+            'Date': pd.to_datetime(['2021-01-01', '2021-01-02']),
+            'Close': [100, 150]
+        })
+        test_score_df = pd.DataFrame({
+            'Date': pd.to_datetime(['2021-01-01', '2021-01-02']),
+            'Close': [100, 150]
+        })
+        train = pd.DataFrame({
+            'Date': pd.to_datetime(['2020-12-30', '2020-12-31']),
+            'Close': [80, 90]
+        })
+        test = pd.DataFrame({
+            'Date': pd.to_datetime(['2021-01-01', '2021-01-02']),
+            'Close': [100, 150]
+        })
+
+        # Call the function to be tested
+        plot_anomaly_points(anomalies, test_score_df, train, test)
+
+        # If the function executes plot commands, the plt.show() method should be called
+        mock_plt.assert_called_once()
+
+    @patch('main.plt.show')
+    def test_plot_train_val_loss(self, mock_plt):
+        # Mock data for history object - adjust as necessary
+        history_mock = Mock()
+        history_mock.history = {'loss': [0.1, 0.2], 'val_loss': [0.15, 0.25]}
+
+        # Call the function to be tested
+        plot_train_val_loss(history_mock)
+
+        # Check if plt.show() was called
+        mock_plt.assert_called_once()
+
+
+class TestDetermineAnomaliesFunction(unittest.TestCase):
+    @patch('main.plt.show')  # Assuming the function is in 'main.py'
+    def test_determine_anomalies(self, mock_plt):
+        # Mocking the model's predict method
+        model_mock = Mock()
+        model_mock.predict.side_effect = lambda x: x + 0.1  # For example, the prediction is the input + some noise
+
+        TIME_STEPS = 30  # This should match the setting in your actual function
+
+        # Generating data for X_train and X_test with an understanding that each sequence is based on TIME_STEPS
+        num_sequences_train = 100  # arbitrary choice
+        num_sequences_test = 20  # arbitrary choice, but should reflect that it's less than the train set
+        X_train = np.random.rand(num_sequences_train, TIME_STEPS, 1)  # (num_sequences, TIME_STEPS, num_features)
+        X_test = np.random.rand(num_sequences_test, TIME_STEPS, 1)
+
+        # Remember, we will "lose" TIME_STEPS - 1 points due to the sliding window
+        test_dates = pd.date_range(start='2021-01-01', periods=num_sequences_test + TIME_STEPS - 1, freq='D')
+        test_close_prices = np.random.uniform(low=100, high=200, size=(num_sequences_test + TIME_STEPS - 1,))
+        test_df = pd.DataFrame({'Date': test_dates, 'Close': test_close_prices})
+
+        # Calling the function with the mock data and model
+        result_test_score_df, result_anomalies = determine_anomalies(model_mock, X_train, X_test, test_df, TIME_STEPS)
+
+        expected_length = num_sequences_test  # since we lose TIME_STEPS - 1 points, and we generated that many extra
+        self.assertEqual(len(result_test_score_df), expected_length)
+
+        # Ensure that the plotting function was called
+        mock_plt.assert_called()
+
+
+if __name__ == '__main__':
+    unittest.main()
